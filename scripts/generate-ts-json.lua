b22559ec51e5f8f01ada54f5e6028a676641782b
Marc Jakobi <marc.jakobi@tiko.energy>
2024-03-10 01:42:04 +0100

    feat: add workflow for updating tree-sitter-parsers.json

---
+++ scripts/generate-ts-json.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env -S nvim -u NONE -U NONE -N -i NONE -l
+
+local home = vim.uv.os_getenv('HOME')
+
+local sc = vim.system({ "luarocks", "install", "--local", "nvim-treesitter" }):wait()
+
+if sc.code ~= 0 then
+  error("Failed to install nvim-treesitter: " .. sc.stderr .. "\n" .. sc.stdout)
+end
+
+local luarocks_path = {
+    vim.fs.joinpath(home, "share", "lua", "5.1", "?.lua"),
+    vim.fs.joinpath(home, "share", "lua", "5.1", "?", "init.lua"),
+}
+package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+
+local luarocks_cpath = {
+    vim.fs.joinpath(home, "lib", "lua", "5.1", "?.so"),
+    vim.fs.joinpath(home, "lib64", "lua", "5.1", "?.so"),
+}
+package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+
+io.write(vim.json.encode(require('nvim-treesitter.parsers').get_parser_configs()))
