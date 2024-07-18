d5350726e5eefff2672439be98405851e9cf4777
Marc Jakobi <marc.jakobi@tiko.energy>
2024-07-18 19:39:46 +0200

    refactor: extract jq script

---
+++ scripts/ts-update-counts.jq
@@ -0,0 +1,15 @@
+def revisions(file): reduce (file[0].parsers[] | {lang: .lang, rev: .install_info.revision}) as $item ({}; .[$item.lang] = $item.rev);
+
+def count_updates(prev; curr; existing_counts):
+  reduce (prev + curr | to_entries | map(.key) | unique | .[]) as $key (
+    existing_counts;
+    if (prev[$key] // null) != (curr[$key] // null) then
+      .[$key] = (existing_counts[$key] // 0) + 1
+    else
+      .[$key] = 1
+    end
+  );
+
+revisions($prev) as $prev_revisions |
+revisions($curr) as $curr_revisions |
+count_updates($prev_revisions; $curr_revisions; $existing_counts) | to_entries | {update_counts: .}
--- scripts/ts-update-counts.sh
+++ scripts/ts-update-counts.sh
@@ -16,20 +16,6 @@ if ! echo "$existing_counts" | jq empty; then
   exit 1
 fi
 
-updates=$(jq -n --argjson existing_counts "$existing_counts" --slurpfile prev prev.json --slurpfile curr tree-sitter-parsers.json '
-  def revisions(file): reduce (file[0].parsers[] | {lang: .lang, rev: .install_info.revision}) as $item ({}; .[$item.lang] = $item.rev);
-  def count_updates(prev; curr; existing_counts):
-    reduce (prev + curr | to_entries | map(.key) | unique | .[]) as $key (
-      existing_counts;
-      if (prev[$key] // null) != (curr[$key] // null) then
-        .[$key] = (existing_counts[$key] // 0) + 1
-      else
-        .[$key] = 1
-      end
-    );
-  revisions($prev) as $prev_revisions |
-  revisions($curr) as $curr_revisions |
-  count_updates($prev_revisions; $curr_revisions; $existing_counts) | to_entries | {update_counts: .}
-')
+updates=$(jq -n -f scripts/ts-update-counts.jq --argjson existing_counts "$existing_counts" --slurpfile prev prev.json --slurpfile curr tree-sitter-parsers.json)
 
 echo "$updates" | jq -S '{update_counts: .update_counts}' > resources/ts-update-counts.json
